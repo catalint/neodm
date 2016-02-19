34ee72fb5b3466bb279e2df76902793cfd850332
Catalin Tanasescu <catalin.tanasescu@kalon.ro>
2016-02-19 17:59:24 +0200

    starting to make tests on dummy server

--- test/node.js
+++ test/node.js
@@ -1,3 +1,46 @@
 'use strict';
 
-require('../src');
+const NeoDB = require('neodb');
+//const Code = require('code');
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+
+//const describe = lab.describe;
+const it = lab.it;
+const before = lab.before;
+const after = lab.after;
+//const expect = Code.expect;
+
+
+const NeoDM = require('../src');
+//const Model = NeoDM.Model;
+
+const db = new NeoDB(6363);
+
+before((done) => {
+
+    db
+        .start()
+        .then((data) => {
+
+            NeoDM.db.setDB(data.url);
+            done();
+        })
+        .catch((err) => done(err));
+});
+
+after((done) => {
+
+    db
+        .stop()
+        .then(() => {
+
+            done();
+        })
+        .catch((err) => done(err));
+});
+
+it('should create a test server', (done) => {
+
+    done();
+});
