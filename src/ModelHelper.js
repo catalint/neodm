dac2b4b861b3c284ad8c8f9a7fd0fb6ff449768e
Catalin Tanasescu <catalin.tanasescu@kalon.ro>
2016-02-24 18:02:42 +0200

    improved tracking of more than 1 relations on hasOne

--- src/ModelHelper.js
+++ src/ModelHelper.js
@@ -105,8 +105,8 @@ class ModelHelper {
 
                     hasOneRels.forEach((rel) => {
 
-                        if (JSON.stringify(prev) === JSON.stringify(cur)) {
-                            throw new Error(`Unexpected relationship has more than 1 result model:${JSON.stringify(from.getModelName())} rel:${JSON.stringify(rel)} prev:${JSON.stringify(prev)} cur:${JSON.stringify(cur)}`);
+                        if (JSON.stringify(prev[rel.key]) === JSON.stringify(cur[rel.key])) {
+                            throw new Error(`Unexpected relationship has more than 1 result model:${JSON.stringify(from.getModelName())} rel:${JSON.stringify(rel)} prev:${JSON.stringify(prev[rel.key])} cur:${JSON.stringify(cur[rel.key])}`);
                         }
                     });
                 }, results[0]);
